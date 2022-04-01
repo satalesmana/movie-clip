c2a3fa0a69cb8a5eeea2374ef313644f95ea0ad1
sata lesmana <lesmanasata@gmail.com>
2022-04-01 19:22:18 +0700

    update login

--- app/src/main/java/com/sata/movieclip/LoginActivity.java
+++ app/src/main/java/com/sata/movieclip/LoginActivity.java
@@ -3,12 +3,34 @@ package com.sata.movieclip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText etEmail, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notifikasiMsg(etEmail.getText().toString()+" | "+ etPassword.getText().toString());
+            }
+        });
+    }
+
+    public void notifikasiMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
