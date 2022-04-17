cee544cfba531f9b5cb9ac2932b531f5327a7634
sata lesmana <lesmanasata@gmail.com>
2022-04-17 22:42:00 +0700

    base repo for UTS

--- app/src/main/java/com/sata/movieclip/MainActivity.java
+++ app/src/main/java/com/sata/movieclip/MainActivity.java
@@ -1,8 +1,13 @@
 package com.sata.movieclip;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_profile:
+                //function untuk pindah ke halaman profile
+                Intent intent = new Intent(this,ProfileActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menu_logout:
+                //buatkan action untuk masuk kehalaman login
+                return true;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
