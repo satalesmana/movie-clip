9576f701f3bbe14699247ebe4962e7fa65d99e27
Sata Lesmana <lesmanasata@gmail.com>
2022-07-01 19:44:08 +0700

    update

--- app/src/main/java/com/sata/movieclip/LoginActivity.java
+++ app/src/main/java/com/sata/movieclip/LoginActivity.java
@@ -1,17 +1,31 @@
 package com.sata.movieclip;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.gesture.GestureOverlayView;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 public class LoginActivity extends AppCompatActivity {
-    Button btnLogin;
-    EditText etEmail, etPassword;
+    private Button btnLogin;
+    private EditText etEmail, etPassword;
+    private SignInButton signInButton;
+    private GoogleSignInClient googleSignInClient;
+    private int RC_SIGN_IN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
+        signInButton = findViewById(R.id.loginGoogle);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +43,20 @@ public class LoginActivity extends AppCompatActivity {
                 onCheckLogin();
             }
         });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN )
+            .requestEmail()
+            .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
     }
 
     private void onCheckLogin(){
@@ -46,5 +75,26 @@ public class LoginActivity extends AppCompatActivity {
     private void notifikasiMsg(String msg) {
         Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
         toast.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                final GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (!account.getId().equals("")&&!account.getId().equals(null)){
+                    Intent i = new Intent(this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(this, "Login Gagal", Toast.LENGTH_SHORT).show();
+                }
+            }catch (ApiException e) {
+                Log.d("error", ""+e);
+                Toast.makeText(this, "Login Gagal", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
